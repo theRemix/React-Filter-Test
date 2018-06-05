c48f88132a6c9e282b44aefd794252fdd8761962
Bjorn Borgonia <bjorn@easci.com>
2018-06-05 16:20:01 -0700

    generate and display random data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { companies, firstNames, lastNames } from './data';
+
+const generateUsers = num => Array(num).fill(null).map((_, i) => ({
+  firstName : firstNames[Math.floor(Math.random() * firstNames.length)],
+  lastName : lastNames[Math.floor(Math.random() * lastNames.length)],
+  company : companies[Math.floor(Math.random() * companies.length)],
+}))
+
+const Row = ({ firstName, lastName, company }) => 
+  <tr>
+    <td>{firstName}</td>
+    <td>{lastName}</td>
+    <td>{company}</td>
+  </tr>;
 
 class App extends Component {
+  state = {
+    users : []
+  }
+
+  componentDidMount() {
+
+    const users = generateUsers(this.props.numUsers);
+    this.setState({ users });
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +35,26 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <select>
+            <option>Select Company</option>
+            { companies.map(c => <option value={c}>{c}</option> ) }
+          </select>
+
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="table-container">
+          <table>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Company</th>
+            </tr>
+
+            { this.state.users.map(user => <Row {...user} /> ) }
+
+          </table>
+        </div>
       </div>
     );
   }
